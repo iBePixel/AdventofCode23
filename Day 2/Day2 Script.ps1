0fc8a38e0dec72c07cceee0a57c6d9feffdf1466
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2023-12-03 23:17:29 +0000

    Update Day2 Script.ps1

--- Day 2/Day2 Script.ps1
+++ Day 2/Day2 Script.ps1
@@ -2,22 +2,22 @@ $input = Get-Content -Path .\documents\'github\adventofcode23\day 2\input.txt'
 $red = 12
 $blue = 14
 $green = 13
+$result = 0
 $games = @()
 
 ForEach($game in $input){
     $games += $game -replace('Game.+:', '')
 }
 
-$games
 
-for($i = 0; $i -lt 100; $i++){
+for($i = 0; $i -lt 99; $i++){
     $CurrGame = $games[$i].split(';')
     $redArr = @()
     $blueArr = @()
     $greenArr = @()
 
     for($j = 0; $j -lt $CurrGame.length; $j++){
-        $splitItems = $currGame[$j] -split',',3
+        $splitItems = $currGame[$j] -split','
         ForEach ($val in $splitItems) {
             if($val -match 'red'){
                 $redArr += $val -replace '\D+'
@@ -28,9 +28,10 @@ for($i = 0; $i -lt 100; $i++){
             elseif($val -match 'blue'){
                 $blueArr += $val -replace '\D+' 
             }
-            Write-Host $redArr
             Write-Host $blueArr
+            Write-Host $redArr
             Write-Host $greenArr
         }
+        
     }
 }

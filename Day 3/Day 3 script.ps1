e8aa6b9205e14c0844a498a4a13594a13af4e021
iBePixel <53214529+iBePixel@users.noreply.github.com>
2023-12-04 16:41:34 +0000

    Update Day 3 script.ps1

--- Day 3/Day 3 script.ps1
+++ Day 3/Day 3 script.ps1
@@ -1,5 +1,5 @@
 $example = Get-Content -Path 'C:\users\ElliottLamb\OneDrive - SquaredUp\documents\github\adventofcode23\day 3\input.txt'
-$symbols = '@|\*|\+|\=|$|/|&|%|-|#'
+$symbols = '\@|\*|\+|\=|\$|\/|\&|\%|\-|\#'
 $result = 0
 
 For ($i =0; $i -lt 140; $i++){
@@ -10,21 +10,26 @@ For ($i =0; $i -lt 140; $i++){
         $numStart = $match.index
         $numEnd = $match.index + $match.length -1
 
-        if ($example[$i -1][($numStart -1)..($numEnd +1)] -match $symbols ){
-            $result += $match.value
+        For ($j= $numStart -1; $j -lt $numEnd +1; $j++){
+            if ($example[$i -1][$j] -match $symbols){
+                $result += $match.value
+                Write-Host "found" + $example[$i -1][$j]
+            }
         }
-        elseif ($example[$i][$numStart -1] -match $symbols){
-            $result += $match.value
-        }
-        elseif ($example[$i][$numEnd +1] -match $symbols){
-            $result += $match.value
-        }
-        elseif ($example[$i + 1][($numEnd -1)..($numEnd +1)]){
-            $result += $match.value
-        }
-        else {
 
-        }
+        #if ($example[$i -1][($numStart)..($numEnd)] -match $symbols ){
+         #   $result += $match.value
+          #  Write-Host "help"
+        #}
+        #elseif ($example[$i][$numStart -1] -match $symbols){
+         #   $result += $match.value
+        #}
+        #elseif ($example[$i][$numEnd +1] -match $symbols){
+         #   $result += $match.value
+        #}
+        #else {
+
+        #}
     }
     Write-Host $result
 }
